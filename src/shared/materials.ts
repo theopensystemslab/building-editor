6c60b173f7ccbecf23bca6262c42b473d7d83102
John Rees <john@bitsushi.com>
2020-05-17 12:23:29 +0100

    start using outline on hangar

--- src/shared/materials.ts
+++ src/shared/materials.ts
@@ -8,15 +8,15 @@ export const wallGhostMaterial = new MeshPhongMaterial({
 });
 
 export const wallMaterial = new MeshPhongMaterial({
-  color: "#666",
-  opacity: 0.8,
+  color: "#53ADF9",
+  opacity: 0.03,
   transparent: true,
   side: DoubleSide,
 });
 
 export const wallMaterialHover = new MeshPhongMaterial({
-  color: "#F2BB05",
-  opacity: 0.8,
+  color: "#53ADF9",
+  opacity: 0.6,
   transparent: true,
   side: DoubleSide,
 });
